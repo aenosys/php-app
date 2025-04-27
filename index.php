7d470975c7d888227bbb031f3ed2c171446ef092
aenosys <aenosys@gmail.com>
2025-04-27 11:40:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Only echo necessary safe outputs
-echo "App is running.\n";
+echo "App is running-TEST.\n";
 
 // Optionally use secrets internally, but don't echo them publicly.
 $my_secret = getenv('testkey1');
